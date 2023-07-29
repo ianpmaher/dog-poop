ba09287b9987c3c17a8e8203d494fa36a6869454
Ian Maher <ianpmaher@gmail.com>
2023-07-29 01:25:27 -0400

    played around with timer more, working on modal

--- app.js
+++ app.js
@@ -31,11 +31,16 @@ window.onload = (event) => {
   // in depth reading here: https://johnresig.com/blog/how-javascript-timers-work/
   // https://www.sitepoint.com/creating-accurate-timers-in-javascript/
 
+  const counterMinElem = document.querySelector("#minutes");
+  counterMinElem.innerHTML = 25 + ":"
+  const counterSecElem = document.querySelector("#seconds")
+  counterSecElem.innerHTML = "00"
   // target date is so many minutes from "now"
-  let targetDatePom = new Date().getTime() + 25 * 60 * 1000;
+  // for target date (milliseconds since epoch), I added the +1000 to allow for delay of 1 second
+  let targetDatePom = new Date().getTime() + 25 * 60 * 1000 + 1000;
   const startTimerPom = () => {
     // const countdown = setInterval(function() {
-    let now = new Date().getTime() + 500;
+    let now = new Date().getTime();
     let timeLeft = targetDatePom - now;
     let mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     let sec = Math.floor((timeLeft % (1000 * 60)) / 1000);
@@ -47,20 +52,38 @@ window.onload = (event) => {
 
   // this seems to work, but with a DELAY. Not sure why the timer starts regardless of button
   // button only seems to display the numbers since page loaded?
+
+  // I am adapting below to allow for buttons to work, I think issue is with scope
+  let timerInterval;
+
   const timerButtonElem = document.querySelector("#timer-button");
   timerButtonElem.addEventListener(
     "click",
     () => {
-      setInterval(startTimerPom, 1000);
+      now = new Date().getTime()
+      timerInterval = setInterval(startTimerPom, 500);
     },
     { once: true }
   );
 
-  // const timerResetButtonElem = document.querySelector("#timer-reset");
+  const timerResetButtonElem = document.querySelector("#timer-reset");
+  timerResetButtonElem.addEventListener(
+    "click",
+    () => {
+      clearInterval(timerInterval);
+      timerInterval = setInterval(startTimerPom, 500);
+    },
+    { once: true }
+  );
 
-  // timerButtonElem.addEventListener("click", )
-
-  // timerResetButtonElem.addEventListener("click", clearInterval(timerFunction));
+  const timerStopButtonElem = document.querySelector("#timer-stop");
+  timerStopButtonElem.addEventListener(
+    "click",
+    () => {
+      clearInterval(timerInterval);
+    },
+    { once: true }
+  );
 
   // function to toggle display of attributions
   const toggleAttributions = () => {
@@ -78,3 +101,28 @@ window.onload = (event) => {
   const attributionsButton = document.querySelector("#attributions-button");
   attributionsButton.addEventListener("click", toggleAttributions);
 };
+
+// modal test adapted from MDN page: https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal
+const modalButton = document.querySelector("#modal-button")
+const cancelButton = document.querySelector("#cancel")
+const modal = document.querySelector("#hidden")
+
+const openDialogCheck = (dialog) => {
+  if (dialog.open) {
+    console.log("dialog open")
+  } else {
+    console.log("dialog closed")
+  }
+}
+
+// modal button opens modal
+modalButton.addEventListener("click", () => {
+  dialog.showModal()
+  openDialogCheck(dialog)
+})
+
+// cancel button closes modal
+cancelButton.addEventListener("click", () => {
+  dialog.close()
+  openDialogCheck(dialog)
+})

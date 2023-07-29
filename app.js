05b050b2468cec8413d19f4dc65d8ab0db80f320
Ian Maher <ianpmaher@gmail.com>
2023-07-29 19:35:50 -0400

    added second API call, formatted content on modal

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.onload = (event) => {
   // declare fetch() function
   const fetchImage = () => {
-    const url = "https://api.thedogapi.com/v1/images/search";
+    const url = "https://api.thedogapi.com/v1/images/search?limit=3";
 
     fetch(url, {
       headers: {
@@ -13,16 +13,64 @@ window.onload = (event) => {
       })
       .then(
         (data) => {
-          imagesData = data[0];
-          document.querySelector("#dog-pic").src = imagesData.url;
-          console.log(imagesData.url);
+          imagesData1 = data[0];
+          imagesData2 = data[1];
+          imagesData3 = data[2]
+          document.querySelector("#dog-pic1").src = imagesData1.url;
+          document.querySelector("#dog-pic2").src = imagesData2.url;
+          document.querySelector("#dog-pic3").src = imagesData3.url;
         },
         (err) => console.log(err)
       );
   };
+  // by default, will be fetching imagae
+  fetchImage();
   // pic button to randomize picture
   const picButton = document.querySelector("#pic-button");
   picButton.addEventListener("click", fetchImage);
+
+  // FETCH DOG FACTS
+  // API is same as above
+  // I am extremely proud I got this to work
+  let arrBreedFacts = [];
+
+  const fetchFact = () => {
+    const url = "https://api.thedogapi.com/v1/breeds";
+
+    fetch(url, {
+      headers: {
+        // "x-api-key": "live_HrMNIJaTAebFXphkPGrvAcwT6K4ojinQBHMbeK0H76tGbXd9at62d4tnrQNCuwFy",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then(
+        (data) => {
+          // filtering out the entries that do not have picture, on documentation of theCatAPI
+          data = data.filter((img) => img.image?.url != null);
+          // for their dog breed json info, they have 264 breeds with unique id numbers (1-264)
+          // this took me SO LONG to get right, but it makes sense to me now. I was getting errors
+          // because I had filtered out entries without images, and my number of random elements was incorrect as a result
+          let randomBreed = Math.floor(Math.random() * data.length)
+          arrBreedFacts = data;
+          document.querySelector("#breed").textContent = String(arrBreedFacts[randomBreed]["name"]);
+          document.querySelector("#example-pic").setAttribute("src", arrBreedFacts[randomBreed].image.url)
+          document.querySelector("#purpose").textContent = String(arrBreedFacts[randomBreed]["bred_for"])
+          document.querySelector("#breed-group").textContent = String(arrBreedFacts[randomBreed]["breed_group"])
+          document.querySelector("#temperament").textContent = String(arrBreedFacts[randomBreed]["temperament"])
+
+          console.log(arrBreedFacts[randomBreed].name);
+        },
+        (err) => console.log(err)
+      );
+  };
+
+  // display a breed's info on default when load
+  fetchFact();
+  // dog fact button to randomize breed
+  const factButton = document.querySelector("#fact-button");
+  factButton.addEventListener("click", fetchFact)
 
   // **** TIMER FUNCTION ****
   // went down rabbit hole based on apparent unreliability of JavaScript's timing functions
@@ -31,59 +79,83 @@ window.onload = (event) => {
   // in depth reading here: https://johnresig.com/blog/how-javascript-timers-work/
   // https://www.sitepoint.com/creating-accurate-timers-in-javascript/
 
-  const counterMinElem = document.querySelector("#minutes");
-  counterMinElem.innerHTML = 25 + ":"
-  const counterSecElem = document.querySelector("#seconds")
-  counterSecElem.innerHTML = "00"
-  // target date is so many minutes from "now"
-  // for target date (milliseconds since epoch), I added the +1000 to allow for delay of 1 second
-  let targetDatePom = new Date().getTime() + 25 * 60 * 1000 + 1000;
-  const startTimerPom = () => {
-    // const countdown = setInterval(function() {
-    let now = new Date().getTime();
-    let timeLeft = targetDatePom - now;
-    let mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    let sec = Math.floor((timeLeft % (1000 * 60)) / 1000);
-    const counterMinElem = document.querySelector("#minutes");
-    counterMinElem.innerHTML = mins.toString() + ":" + (sec < 10 ? "0" : "");
-    const counterSecElem = document.querySelector("#seconds");
-    counterSecElem.innerHTML = String(sec);
+  // I found this to be the simpler way of constructing user buttons that will start the counter
+  const userTimeInputArr = [25, 10, 5];
+
+  let userSelect25 = document.querySelector("#user-time-pom");
+  userSelect25.addEventListener("click", () => {
+    sessionAmount = userTimeInputArr[0];
+    pomTimer();
+  });
+  let userSelect10 = document.querySelector("#user-time-10");
+  userSelect10.addEventListener("click", () => {
+    sessionAmount = userTimeInputArr[1];
+    pomTimer();
+  });
+  let userSelect5 = document.querySelector("#user-time-5");
+  userSelect5.addEventListener("click", () => {
+    sessionAmount = userTimeInputArr[2];
+    pomTimer();
+  });
+
+  let pomInterval;
+  let state = true;
+
+  const minsElem = document.querySelector("#minutes");
+  minsElem.textContent = "00";
+  const secElem = document.querySelector("#seconds");
+  secElem.textContent = "00";
+
+  const pomTimer = () => {
+    if (state) {
+      state = false;
+      let totalSeconds = sessionAmount * 60;
+
+      const updateSeconds = () => {
+        // Function code here.
+        // const minsElem = document.querySelector("#minutes");
+        // const secElem = document.querySelector("#seconds");
+
+        totalSeconds--;
+
+        let minutesLeft = Math.floor(totalSeconds / 60);
+        let secondsLeft = totalSeconds % 60;
+
+        if (secondsLeft < 10) {
+          secElem.textContent = "0" + secondsLeft;
+        } else {
+          secElem.textContent = secondsLeft;
+        }
+        minsElem.textContent = `${minutesLeft}`;
+
+        if (minutesLeft === 0 && secondsLeft === 0) {
+          clearInterval(pomInterval);
+        }
+      };
+      pomInterval = setInterval(updateSeconds, 1000);
+    } else {
+      console.log("already selected");
+    }
   };
+
+  const timerResetButtonElem = document.querySelector("#timer-reset");
+  timerResetButtonElem.addEventListener("click", () => {
+    clearInterval(pomInterval);
+    minsElem.textContent = "00";
+    secElem.textContent = "00";
+    pomTimer();
+  });
+
+  // const timerStopButtonElem = document.querySelector("#timer-stop");
+  // timerStopButtonElem.addEventListener("click", () => {
+  //   clearInterval(pomInterval);
+  //   pomTimer(25);
+  // });
 
   // this seems to work, but with a DELAY. Not sure why the timer starts regardless of button
   // button only seems to display the numbers since page loaded?
 
   // I am adapting below to allow for buttons to work, I think issue is with scope
-  let timerInterval;
-
-  const timerButtonElem = document.querySelector("#timer-button");
-  timerButtonElem.addEventListener(
-    "click",
-    () => {
-      now = new Date().getTime()
-      timerInterval = setInterval(startTimerPom, 500);
-    },
-    { once: true }
-  );
-
-  const timerResetButtonElem = document.querySelector("#timer-reset");
-  timerResetButtonElem.addEventListener(
-    "click",
-    () => {
-      clearInterval(timerInterval);
-      timerInterval = setInterval(startTimerPom, 500);
-    },
-    { once: true }
-  );
-
-  const timerStopButtonElem = document.querySelector("#timer-stop");
-  timerStopButtonElem.addEventListener(
-    "click",
-    () => {
-      clearInterval(timerInterval);
-    },
-    { once: true }
-  );
 
   // function to toggle display of attributions
   const toggleAttributions = () => {
@@ -105,24 +177,24 @@ window.onload = (event) => {
 // modal test adapted from MDN page: https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal
 // and from https://www.freecodecamp.org/news/how-to-build-a-modal-with-javascript/
 
-const modalElem = document.querySelector(".modal")
+const modalElem = document.querySelector(".modal");
 // obscuring everything in background
-const fillerElem = document.querySelector(".filler")
-const modalButton = document.querySelector("#modal-button")
-const cancelButton = document.querySelector("#cancel")
+const fillerElem = document.querySelector(".filler");
+const modalButton = document.querySelector("#modal-button");
+const cancelButton = document.querySelector("#cancel");
 
 const modalOpen = () => {
-  modalElem.classList.remove("hidden")
-  fillerElem.classList.remove("hidden")
-}
+  modalElem.classList.remove("hidden");
+  fillerElem.classList.remove("hidden");
+};
 
-modalButton.addEventListener("click", modalOpen)
+modalButton.addEventListener("click", modalOpen);
 
 const closeModal = () => {
-  modalElem.classList.add("hidden")
-  fillerElem.classList.add("hidden")
-}
+  modalElem.classList.add("hidden");
+  fillerElem.classList.add("hidden");
+};
 
-cancelButton.addEventListener("click", closeModal)
+cancelButton.addEventListener("click", closeModal);
 // to allow modal to close when clicked outside of it
-fillerElem.addEventListener("click", closeModal)
+fillerElem.addEventListener("click", closeModal);
